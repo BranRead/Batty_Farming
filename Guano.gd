52457124b3fccf5ee6ee3d329723fd8077602690
BranRead <branrtread@gmail.com>
2024-01-20 14:01:38 -0400

    Bats don't leave the screen, they get bigger when they eat food and they drop guano which can be collected

---
+++ Guano.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+export var falling_speed : float = 80
+var name_check : String = "guano"
+var is_selected : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.position.y += falling_speed * delta
+	
+	if Input.is_action_just_pressed("click") && is_selected:
+		#print("Clicking Guano")
+		get_parent().guano_collected += 1
+		#print("Guano collected: " + str(get_parent().guano_collected))
+		self.queue_free()
+
+func _on_Guano_mouse_entered():
+	is_selected = true
+
+func _on_Guano_mouse_exited():
+	is_selected = false
