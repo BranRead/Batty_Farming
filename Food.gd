b259597a4952d5228bc90d081ef94a7ed8159ed6
BranRead <branrtread@gmail.com>
2024-01-20 21:23:10 -0400

    Fixed sound for collecting bat guano
    Sounds may change

--- Food.gd
+++ Food.gd
@@ -4,6 +4,7 @@ export var falling_speed : float = 80
 var name_check : String = "food";
 var cost : float = 0.05
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
--- Guano.gd
+++ Guano.gd
@@ -4,6 +4,8 @@ export var falling_speed : float = 80
 var name_check : String = "guano"
 var is_selected : bool = false
 var worth : float = 0.25
+onready var collect_sound : AudioStreamPlayer = $CollectSound
+var is_fallen_off_screen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,14 +17,17 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("click") && is_selected:
 		#print("Clicking Guano")
-		get_parent().guano_collected += 1
-		get_parent().money += self.worth
-		get_parent().get_node("MoneyLabel").update_money()
-		#print("Guano collected: " + str(get_parent().guano_collected))
-		self.queue_free()
+		collect_guano()
 
 func _on_Guano_mouse_entered():
 	is_selected = true
 
 func _on_Guano_mouse_exited():
 	is_selected = false
+
+func collect_guano():
+	get_parent().guano_collected += 1
+	get_parent().money += self.worth
+	get_parent().get_node("MoneyLabel").update_money()
+	#print("Guano collected: " + str(get_parent().guano_collected))
+	self.queue_free()
